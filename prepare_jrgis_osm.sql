731fd0a3bed8598de7c16797847a0007c79cdd39
0red <0red@users.noreply.github.com>
2021-04-13 23:47:45 +0200

    Update prepare_jrgis_osm.sql

--- prepare_jrgis_osm.sql
+++ prepare_jrgis_osm.sql
@@ -38,7 +38,7 @@ DECLARE
 	RAISE NOTICE 'DROP TIME %',clock_timestamp() - t1;
 	
 	RAISE NOTICE 'drop table finishes (%)',clock_timestamp() - t;
-	RAISE NOTICE 'jrgis.createtables()';
+	RAISE NOTICE 'jrgis.createtables(); - use for tables creation';
 
 END;	
 $$ LANGUAGE plpgsql;
@@ -52,6 +52,7 @@ DECLARE
 	t1 timestamptz ;
 	
  BEGIN
+	RAISE NOTICE 'jrgis.createtables() START';
 	RAISE NOTICE 'pl_forest_poly';
 	t1 := clock_timestamp();
 create table jrgis.pl_forest_poly as 
@@ -65,7 +66,8 @@ create index pl_forest_poly_pl_gdx on jrgis.pl_forest_poly USING gist (way);
 	t1 := clock_timestamp();
 create table jrgis.pl_railway_poly as 
 	select osm_id,name,railway,access,ref,way_area,tags,way from planet_osm_polygon where railway is not null;
-create index pl_railway_poly_idx on jrgis.pl_railway_poly(osm_id);	RAISE NOTICE 'DROP TIME %',clock_timestamp() - t1;
+create index pl_railway_poly_idx on jrgis.pl_railway_poly(osm_id);	
+RAISE NOTICE 'TIME %',clock_timestamp() - t1;
 	
 	RAISE NOTICE 'pl_admin_poly';
 	t1 := clock_timestamp();
@@ -74,7 +76,8 @@ create table jrgis.pl_admin_poly as
 	boundary='administrative' ;
 create index pl_admin_poly_idx on jrgis.pl_admin_poly(osm_id);
 create index pl_admin_poly_gdx on jrgis.pl_admin_poly USING gist (way);
-create index pl_admin_poly_ldx on jrgis.pl_admin_poly(osm_id,admin_level);	RAISE NOTICE 'DROP TIME %',clock_timestamp() - t1;
+create index pl_admin_poly_ldx on jrgis.pl_admin_poly(osm_id,admin_level);
+RAISE NOTICE 'TIME %',clock_timestamp() - t1;
 	
 	RAISE NOTICE 'pl_railway';
 	t1 := clock_timestamp();
@@ -83,7 +86,8 @@ create index pl_admin_poly_ldx on jrgis.pl_admin_poly(osm_id,admin_level);	RAISE
 create index pl_railway_idx on jrgis.pl_railway(osm_id);
 create index pl_railway_gdx on jrgis.pl_railway USING gist (way);
 create index pl_railway_adx on jrgis.pl_railway(osm_id,railway);
-create index pl_railway_rdx on jrgis.pl_railway(osm_id,ref);	RAISE NOTICE 'DROP TIME %',clock_timestamp() - t1;
+create index pl_railway_rdx on jrgis.pl_railway(osm_id,ref);
+RAISE NOTICE 'TIME %',clock_timestamp() - t1;
 	
 	RAISE NOTICE 'pl_highways';
 	t1 := clock_timestamp();
@@ -104,7 +108,7 @@ create index pl_highways_idx on jrgis.pl_highways(osm_id);
 create index pl_highways_gdx on jrgis.pl_highways USING gist (way);
 create index pl_highways_ldx on jrgis.pl_highways(osm_id,level);
 create index pl_highways_rdx on jrgis.pl_highways(osm_id,ref);
-	RAISE NOTICE 'DROP TIME %',clock_timestamp() - t1;
+RAISE NOTICE 'TIME %',clock_timestamp() - t1;
 	
 	RAISE NOTICE 'pl_water_poly';
 	t1 := clock_timestamp();
@@ -113,10 +117,11 @@ select osm_id,water,waterway,name,ref,way_area,tags,way from planet_osm_polygon 
 "natural"='water' ;
 create index pl_water_poly_idx on jrgis.pl_water_poly(osm_id);
 create index pl_water_poly_gdx on jrgis.pl_water_poly USING gist (way);
-create index pl_water_poly_wdx on jrgis.pl_water_poly(osm_id,water);	RAISE NOTICE 'DROP TIME %',clock_timestamp() - t1;
+create index pl_water_poly_wdx on jrgis.pl_water_poly(osm_id,water);	
+RAISE NOTICE 'TIME %',clock_timestamp() - t1;
 	
-	RAISE NOTICE 'drop table finishes (%)',clock_timestamp() - t;
-	RAISE NOTICE 'jrgis.createtables()';
+	RAISE NOTICE 'create table finishes (%)',clock_timestamp() - t;
+	RAISE NOTICE 'jrgis.createtables() END';
 
 END;	
 $$ LANGUAGE plpgsql;
